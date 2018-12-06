42773901e0cc34ab58f3ea7dd0ee622add3f1fc1
trinanda <zidanecr7kaka@gmail.com>
2018-12-06 22:29:54 +0700

    design menu login

--- App.js
+++ App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation'; // Version can be specified in package.json
+import React, { Component } from 'react';
+import { Container, Header, Content, Form, Item, Input, Label, Button, Text, InputText } from 'native-base';
+import { Image } from 'react-native'
 
-import Homepage from './src/screens/Homepage.js'
-import Messages from './src/screens/Messages.js'
-import Menu from './src/screens/Menu.js'
-
-const AppNavigator = createStackNavigator({
-  Homepage: {
-    screen: Homepage,
-  },
-  Messages: {
-    screen: Messages,
-  },
-  Menu: {
-    screen: Menu,
-  },
-
-}, {
-    initialRouteName: 'Homepage',
-    headerMode: "None"
-});
-
-export default createAppContainer(AppNavigator);
+export default class Login extends Component {
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content style = {{backgroundColor: '#3b5998'}}>
+          <Image source = {require('./src/icons/facebook.jpg')} style={{height: 50, width: 50, marginTop: 60, alignSelf: 'center'}}/>
+          <Form style = {{ marginTop: 30, paddingLeft: 30, paddingRight: 30 }}>
+            <Item floatingLabel last>
+              <Input placeholder='Username' placeholderTextColor='#fff' style = {{color: '#fff', fontFamily: 'Helvetica', width: 20}}/>
+            </Item>
+            <Item floatingLabel last>
+              <Input placeholder='Password' placeholderTextColor='#fff' style = {{color: '#fff', fontFamily: 'Helvetica', width: 20}}/>
+            </Item>
+            <Button block style = {{marginTop: 15, backgroundColor: 'rgb(139,157,195)'}}>
+              <Text style = {{ fontFamily: 'Helvetica'}}>Login</Text>
+            </Button>
+          </Form>
+          <Container style = {{backgroundColor: '#3b5998', paddingTop: 50}}>
+            <Text style = {{ color: '#fff', fontSize: 14, alignSelf: 'center' }}>Sign Up for Facebook</Text>
+            <Text></Text>
+            <Text style = {{ color: '#fff', fontSize: 14, alignSelf: 'center' }}>Forgot Password?</Text>
+          </Container>
+      </Content>
+      </Container>
+    );
+  }
+}
