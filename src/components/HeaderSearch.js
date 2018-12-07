c71c1d7b604406218f3092d5600c1a36718525fe
trinanda <zidanecr7kaka@gmail.com>
2018-12-07 16:35:20 +0700

    remove headertile because that has a same class in headersearch

--- src/components/HeaderSearch.js
+++ src/components/HeaderSearch.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react'
 import { Header, Input, Body, Right, Icon, Left, Item } from 'native-base'
 import { TextInput, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
 
-
 export default class HeaderSearch extends Component{
   render(){
     return(
-
       <Header style={{ backgroundColor: '#4267B2' }}>
      		<Left style={{ flex: 1 }}>
           <TouchableOpacity>
--- src/screens/ProfileScreens/ProfilePage.js
+++ src/screens/ProfileScreens/ProfilePage.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react'
 import { View, Text, ScrollView } from 'react-native'
 
 import HeaderTitle from './HeaderTitle.js'
-
+import TimelineCover from './TimelineCover.js'
+import HeaderSearch from '../../components/HeaderSearch.js';
 export default class ProfilePage extends Component {
   render(){
     return(
       <View>
-        <HeaderTitle placeholder='Search' />
+        <HeaderSearch placeholder='Search' navigationsData = {this.props.navigation}/>
+        <ScrollView>
+          <TimelineCover />
+        </ScrollView>
       </View>
     )
   }
